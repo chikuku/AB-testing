07083af99cd9c3c3c242cfe4093d8c979c6be093
YingtzuChen <colorfulday0123@gmail.com>
2025-01-10 13:54:45 -0600

    Update R script: add part 3

--- github_videoCase.R
+++ github_videoCase.R
@@ -4,12 +4,50 @@
 
 library(readxl)
 # load the data from 3 sheets
-df_randomCk <- read_excel("Data(2).xlsx", sheet="Random Check")
-df_corProduct <- read_excel("Data(2).xlsx", sheet="CP Analysis data")
-df_focProduct <- read_excel("Data(2).xlsx", sheet="FP Analysis data")
+df_randomCk <- read_excel("Data_clean.xlsx", sheet="Random Check")
+df_corProduct <- read_excel("Data_clean.xlsx", sheet="CP Analysis data")
+df_focProduct <- read_excel("Data_clean.xlsx", sheet="FP Analysis data")
 
-# Part 1: Check the Randomization of control & treatment group using a t-test
+#### Part 1: Check the Randomization of control & treatment group using a t-test ####
 control_price <- df_randomCk$ProdPrice[df_randomCk$Vid == 0]
 treatment_price <- df_randomCk$ProdPrice[df_randomCk$Vid == 1]
 t_test <- t.test(control_price,treatment_price)
 print(t_test)
+
+#### Part 2: Videos adding value Causal Impact Evaluation ####
+# Also, we can comapre videos with other promotion results in the summary
+# 2-1 For focal products
+# Exclude PostVidWk because VidWk, PreVidWk, and PostVidWk variables are mutually 
+# exclusive dummy variables
+model_fp <- lm(Sales ~ Vid + VidWk + PreVidWk+
+                 ProdCat+ ProdPrice + PriceDiscWk + 
+                 EmailWk + CatalogWk + HomePgWk + CatPgWk, data = df_focProduct)
+summary(model_fp)
+# 2-2 For coordinating products
+# Exclude PostVidWk because VidWk, PreVidWk, and PostVidWk variables are mutually 
+# exclusive dummy variables
+model_cor <- lm(CpSales ~ VidWk + PreVidWk+
+                  FpPriceDiscWk +	FpEmailWk +	FpCatalogWk	+
+                  FpHomePgWk	+ FpCatPgWk +	CpPriceDiscWk +	
+                  CpEmailWk +	CpCatalogWk	+ CpHomePgWk	+
+                  CpCatPgWk, data = df_corProduct)
+summary(model_cor)
+
+#### Part 3: Evaluating the Impact of Video and Integrated Marketing Strategies####
+# 3-1 For focal products
+# Exclude PostVidWk because VidWk, PreVidWk, and PostVidWk variables are mutually 
+# exclusive dummy variables
+model_fp <- lm(Sales ~ VidWk *(PreVidWk+
+                 ProdCat+ ProdPrice + PriceDiscWk + 
+                 EmailWk + CatalogWk + HomePgWk + CatPgWk), data = df_focProduct)
+summary(model_fp)
+# 3-2 For coordinating products
+# Exclude PostVidWk because VidWk, PreVidWk, and PostVidWk variables are mutually 
+# exclusive dummy variables
+model_cor <- lm(CpSales ~ VidWk *(PreVidWk+
+                  FpPriceDiscWk +	FpEmailWk +	FpCatalogWk	+
+                  FpHomePgWk	+ FpCatPgWk +	CpPriceDiscWk +	
+                  CpEmailWk +	CpCatalogWk	+ CpHomePgWk	+
+                  CpCatPgWk), data = df_corProduct)
+summary(model_cor)
+
